1ef978c98861c6d89da6020698896ee355fa2cf6
keyflight <olivier@andev.nl>
2019-01-23 15:58:30 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,16 +1,37 @@
-# list-view
+# ![ListView](https://user-images.githubusercontent.com/44801418/48134118-91f82f80-e2cb-11e8-9928-c54c3d41015f.png) ListView
 
-[![Travis][build-badge]][build]
 [![npm package][npm-badge]][npm]
-[![Coveralls][coveralls-badge]][coveralls]
 
-Describe list-view here.
+Used for displaying records as individual items in a list.
 
-[build-badge]: https://img.shields.io/travis/user/repo/master.png?style=flat-square
-[build]: https://travis-ci.org/user/repo
+![ListView](https://user-images.githubusercontent.com/44947294/51593386-5b0cdf80-1ef2-11e9-8ce3-905b6754b2a6.gif)
+## Getting started
 
-[npm-badge]: https://img.shields.io/npm/v/npm-package.png?style=flat-square
-[npm]: https://www.npmjs.org/package/npm-package
+````
+npm install @cmds/list-view --save
+````
 
-[coveralls-badge]: https://img.shields.io/coveralls/user/repo/master.png?style=flat-square
-[coveralls]: https://coveralls.io/github/user/repo
+### Prop Types
+
+| Property | Type | Required? | Description |
+|:---|:---|:---:|:---|
+| id | String | ✓ | Unique id for the instance of this view |
+| rowCount | Number | ✓ | The amount of rows to be rendered |
+| rowGetter | Function | ✓ | Return row data for the index `({index: number})` |
+| fieldRenderer | Function | ✓ | Callback invoked whenever one of the cells get's rendered: `({index: number}): jsx` |
+| fields | Array | ✓ | Array of fields to be displayed as columns |
+
+#### Field
+| Property | Type | Required? | Description |
+|:---|:---|:---:|:---|
+| id | String | ✓ | Unique id for the field |
+| name | String | ✓ | Name to be displayed as column name |
+
+### More information
+
+This component is designed and developed as part of [Cosmos Design System][cmds]. 
+
+[cmds]: https://github.com/entercosmos/cosmos
+[npm-badge]: https://img.shields.io/npm/v/@cmds/list-view.svg
+[npm]: https://www.npmjs.org/package/@cmds/list-view
+
--- src/index.js
+++ src/index.js
@@ -1,9 +1,100 @@
-import React, {Component} from 'react'
+import React from 'react'
+import {css} from 'emotion'
+import PropTypes from 'prop-types'
+import {List, AutoSizer} from 'react-virtualized'
+import 'react-virtualized/styles.css'
 
-export default class extends Component {
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
-  }
+export default class ListView extends React.Component {
+
+    static propTypes = {
+        id: PropTypes.string.isRequired,
+        rowCount: PropTypes.number.isRequired,
+        rowGetter: PropTypes.func.isRequired,
+        fieldRenderer: PropTypes.func.isRequired,
+        fields: PropTypes.arrayOf(
+            PropTypes.shape({
+                id: PropTypes.string.isRequired,
+                name: PropTypes.string.isRequired
+            })
+        )
+    }
+
+    render() {
+
+        return (
+            <AutoSizer>
+                {({width, height}) => (
+                    <List
+                        width={width}
+                        height={height}
+                        rowCount={this.props.rowCount}
+                        rowHeight={118}
+                        rowRenderer={this.rowRenderer}
+                        style={{
+                            paddingTop: 8,
+                            paddingBottom: 8
+                        }}
+                    />
+                )}
+            </AutoSizer>
+        )
+    }
+
+    cellRenderer = (params) => {
+
+        const {columnIndex, rowData, cellData} = params
+
+        const field = this.props.fields[columnIndex]
+
+        return (
+            <div
+                className={css`
+                    width: 100%;
+                    max-width: 100%;
+                    text-overflow: ellipsis;
+                    white-space: nowrap;
+                    line-height: 1.5;
+                    -webkit-box-align: center;
+                    align-items: center;
+                    display: inline-flex;
+                    cursor: pointer;
+                    height: 24px;
+                    overflow: hidden;
+                `}
+            >
+                {this.props.fieldRenderer({
+                    id: rowData.id,
+                    field,
+                    cell: cellData,
+                    index: columnIndex,
+                    props: {
+                        id: field.id,
+                        contextId: 'recordListItem',
+                        roleId: 'readOnly'
+                    }
+                })}
+            </div>
+        )
+    }
+
+    rowRenderer = ({
+                       key,         // Unique key within array of rows
+                       index,       // Index of row within collection
+                       isScrolling, // The List is currently being scrolled
+                       isVisible,   // This row is visible within the List (eg it is not an overscanned row)
+                       style        // Style object to be applied to row (to position it)
+                   }) => (
+        <div
+            key={key}
+            style={style}
+            className={css`
+                padding: 8px 16px;
+            `}
+        >
+
+            {this.props.rowRenderer({
+                index
+            })}
+        </div>
+    )
 }
